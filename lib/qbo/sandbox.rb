9cd7cb1ec35d39e0744ade2553f58ca888d6314d
Dirk Elmendorf <dirk@r26d.com>
2019-01-22 07:18:31 -0600

    Reworking to be a copy

--- lib/qbo/sandbox.rb
+++ lib/qbo/sandbox.rb
@@ -3,20 +3,20 @@ require "qbo_api"
 
 module Qbo
   class Sandbox
-    attr_reader :prod, :sandbox
+    attr_reader :from_connection, :to_connection
 
-    def initialize(production_connection:, sandbox_connection:)
-        @prod = production_connection
-        @sandbox = sandbox_connection
-        prod.connect unless prod.good?
-        sandbox.connect unless sandbox.good?
+    def initialize(from_connection:, to_connection:)
+        @from_connection = from_connection
+        @to_connection = to_connection
+        @from_connection.connect unless @from_connection.good?
+        @to_connection.connect unless @to_connection.good?
     end
 
 
     def copy(entity, max: 1000, select: nil, inactive: false, batch_size: 30)
       batch = []
-      QboApi.production = true
-      prod.all(entity, max: max, select: select, inactive: inactive) do |e|
+      QboApi.production = from_connection.production?
+      from_connection.all(entity, max: max, select: select, inactive: inactive) do |e|
         batch << build_single_batch(e, entity)
       end
       submit_batch(batch, batch_size)
@@ -25,11 +25,11 @@ module Qbo
     private
 
       def submit_batch(batch, batch_size)
-        QboApi.production = false
+        QboApi.production = false #Never copies to production
         collect_responses = []
         batch.each_slice(batch_size) do |b|
           payload = {"BatchItemRequest" => b}
-          collect_responses << sandbox.batch(payload)
+          collect_responses << to_connection.batch(payload)
         end
         collect_responses
       end
@@ -39,7 +39,7 @@ module Qbo
         {
           "bId" => "bid#{e.delete('Id')}",
           "operation" => "create",
-          prod.singular(entity) => e
+          from_connection.singular(entity) => e
         }
       end
 
